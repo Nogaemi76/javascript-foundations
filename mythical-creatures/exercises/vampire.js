03a2b1770fdcd469eaa6d8698c319aa7a17ad14e
Emilie <milogare@yahoo.fr>
2020-11-10 22:37:52 +0100

    Completed Mythical creature : Vampire

---
+++ mythical-creatures/exercises/vampire.js
@@ -0,0 +1,23 @@
+class Vampire {
+    constructor(name, pet) {
+        this._name = name;
+        this._pet = pet || "bat";
+        this._thirsty = true;
+    }
+    get name() {
+        return this._name;
+    }
+    get pet() {
+        return this._pet;
+    }
+
+    get thirsty() {
+        return this._thirsty;
+    }
+    drink() {
+        this._thirsty = false;
+    }
+
+}
+
+module.exports = Vampire;
